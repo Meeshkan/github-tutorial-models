4e8552679e73e9483172bfa3f6f4f92d3eb74b58
Mike Solomon <mike@mikesolomon.org>
2018-02-20 20:51:16 +0200

    Makes model size smaller for faster exploration

--- gen_models.py
+++ gen_models.py
@@ -8,12 +8,14 @@ input_dim = int(sys.argv[1])
 model_name = 'my_model' if len(sys.argv) == 2 else sys.argv[2]
 
 NETWORKS = [
-    [2400, 1200, 800, 400, 200, 50, 2],
-    [1200, 800, 400, 200, 50, 2],
     [800, 400, 200, 50, 2],
     [400, 200, 50, 2],
     [200, 50, 2],
-    [50, 2]
+    [50, 2],
+    [800, 400, 200, 50, 1],
+    [400, 200, 50, 1],
+    [200, 50, 1],
+    [50, 1]
 ]
 
 for NETWORK in NETWORKS:
@@ -23,7 +25,7 @@ for NETWORK in NETWORKS:
             model.add(Dense(NETWORK[i], input_dim=input_dim, activation='relu'))
         else :
             model.add(Dense(NETWORK[i], activation='relu' if i < len(NETWORK) - 1 else 'sigmoid'))
-    model.compile(loss=keras.losses.categorical_crossentropy,
+    model.compile(loss=keras.losses.categorical_crossentropy if NETWORK[-1] == 2 else keras.losses.mean_squared_error,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
     model.save('{0}_{1}.h5'.format(model_name, '_'.join([str(n) for n in NETWORK])))
